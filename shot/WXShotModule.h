dcb1a14ff9dacec6ddd44937591ee1df69973846
郑江荣 <362675035@qq.com>
2019-02-15 23:09:47 +0800

    first

---
+++ shot/WXShotModule.h
@@ -0,0 +1,16 @@
+//
+//  WXShotModule.h
+//  AFNetworking
+//
+//  Created by 郑江荣 on 2019/2/15.
+//
+
+#import <Foundation/Foundation.h>
+#import <WeexSDK/WeexSDK.h>
+
+
+@interface WXShotModule :  NSObject<WXModuleProtocol>
+
+@end
+
+ 
---
+++ shot/shot.m
@@ -0,0 +1,13 @@
+//
+//  shot.m
+//  shot
+//
+//  Created by 郑江荣 on 2019/2/15.
+//  Copyright © 2019 郑江荣. All rights reserved.
+//
+
+#import "shot.h"
+
+@implementation shot
+
+@end
